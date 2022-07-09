8fe77d260f9a0bb605b54bd6a71f1bd2780343bd
krleitch <krleitch.ca@gmail.com>
2022-07-09 11:20:35 -0400

    prod setup

--- config/runtime.exs
+++ config/runtime.exs
@@ -26,7 +26,8 @@ if config_env() == :prod do
     # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
+    types: SpotChat.PostgresTypes
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
@@ -40,7 +41,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "spottables.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :spot_chat, SpotChatWeb.Endpoint,
